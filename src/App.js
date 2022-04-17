aa34d6b1f2e5a953aabac90019f66b03fdfe411e
Pablo Velásquez Rivas <pablovelasquezr@gmail.com>
2022-04-17 02:19:27 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import html2canvas from 'html2canvas';
 
 function App() {
+
+  const [linea1, setLinea1] = useState('');
+  const [linea2, setLinea2] = useState('');
+  const [imagen, setImagen] = useState('');
+
+  const onChangeLinea1 = function (evento) {
+    setLinea1(evento.target.value);
+  }
+
+  const onChangeLinea2 = function (evento) {
+    setLinea2(evento.target.value);
+  }
+
+  const onChangeLineaImg = function (evento) {
+    setImagen(evento.target.value);
+  }
+
+  const onClickExportar = function (evento) {
+    html2canvas(document.querySelector("#meme")).then(canvas => {
+      var img = canvas.toDataURL("image/png");
+      var link = document.createElement('a');
+      link.download = 'meme.png';
+      link.href = img;
+      link.click();
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select onChange={onChangeLineaImg}>
+        <option value="Forever-Alone.jpg">Forever Alone</option>
+        <option value="sapo.jpg">Sapo</option>
+        <option value="brian.jpg">Brian</option>
+        <option value="Ancient-Aliens.jpg">Ancient Aliens</option>
+      </select> <br />
+
+      <input maxlength="11" onChange={onChangeLinea1} placeholder='Top Text' type="text"/><br />
+      <input maxlength="11" onChange={onChangeLinea2} placeholder='Bottom Text' type="text"/><br />
+      <div className='meme' id='meme'>
+        <span>{linea1}</span><br />
+        <img src={"/img/" + imagen}/> <br />
+        <span className="text">{linea2}</span><br />
+        
+      </div>
+      <button onClick={onClickExportar}>Exportar</button>
     </div>
   );
 }
